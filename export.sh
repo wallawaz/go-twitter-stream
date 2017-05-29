95e6a643bdc515631f0edb0f24ee94f4b38c1935
ben <bwallad@gmail.com>
2017-05-29 10:11:11 -0400

    add twitter env export

---
+++ export.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Should be sourced from terminal `source export.sh`
+
+export TWITTER_CONSUMER_KEY=<...>
+export TWITTER_CONSUMER_SECRET=<...>
+export TWITTER_ACCESS_TOKEN=<...>
+export TWITTER_ACCESS_SECRET=<...>
+
